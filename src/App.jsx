c2f27731714aec57f8f0c2113a99138e2a1cdb70
pattrick2 <johnpatrickpar409@gmail.com>
2025-05-31 17:57:26 +0800

    3

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
+import React, { useState } from 'react'
+import './App.css'
+import Todolist from './assets/todolist.jsx'
+//import Todolist from'./todolist.jsx';
+// import { Images } from "./assets"
+// import todolist from'./todolist.jsx'
+
+function App() {
+
+  return(<Todolist />);
+}
+
+export default App
